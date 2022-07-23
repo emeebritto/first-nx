93c7c7c47d0fdd848216b0c18f708d1df30df7df
Emerson-Britto <emersonbritto987@gmail.com>
2022-07-23 14:58:23 -0300

    [refactor]: indetation

--- train.py
+++ train.py
@@ -10,23 +10,23 @@ from utils.nltk_utils import bag_of_words, tokenize, stem
 from models.model import NeuralNet
 
 with open('data/intents.json', 'r') as f:
-    intents = json.load(f)
+  intents = json.load(f)
 
 all_words = []
 tags = []
 xy = []
 # loop through each sentence in our intents patterns
 for intent in intents['intents']:
-    tag = intent['tag']
-    # add to tag list
-    tags.append(tag)
-    for pattern in intent['patterns']:
-        # tokenize each word in the sentence
-        w = tokenize(pattern)
-        # add to our words list
-        all_words.extend(w)
-        # add to xy pair
-        xy.append((w, tag))
+  tag = intent['tag']
+  # add to tag list
+  tags.append(tag)
+  for pattern in intent['patterns']:
+    # tokenize each word in the sentence
+    w = tokenize(pattern)
+    # add to our words list
+    all_words.extend(w)
+    # add to xy pair
+    xy.append((w, tag))
 
 # stem and lower each word
 ignore_words = ['?', '.', '!']
@@ -43,12 +43,12 @@ print(len(all_words), "unique stemmed words:", all_words)
 X_train = []
 y_train = []
 for (pattern_sentence, tag) in xy:
-    # X: bag of words for each pattern_sentence
-    bag = bag_of_words(pattern_sentence, all_words)
-    X_train.append(bag)
-    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
-    label = tags.index(tag)
-    y_train.append(label)
+  # X: bag of words for each pattern_sentence
+  bag = bag_of_words(pattern_sentence, all_words)
+  X_train.append(bag)
+  # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
+  label = tags.index(tag)
+  y_train.append(label)
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
@@ -64,24 +64,26 @@ print(input_size, output_size)
 
 class ChatDataset(Dataset):
 
-    def __init__(self):
-        self.n_samples = len(X_train)
-        self.x_data = X_train
-        self.y_data = y_train
+  def __init__(self):
+    self.n_samples = len(X_train)
+    self.x_data = X_train
+    self.y_data = y_train
 
-    # support indexing such that dataset[i] can be used to get i-th sample
-    def __getitem__(self, index):
-        return self.x_data[index], self.y_data[index]
+  # support indexing such that dataset[i] can be used to get i-th sample
+  def __getitem__(self, index):
+    return self.x_data[index], self.y_data[index]
 
-    # we can call len(dataset) to return the size
-    def __len__(self):
-        return self.n_samples
+  # we can call len(dataset) to return the size
+  def __len__(self):
+    return self.n_samples
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=0)
+train_loader = DataLoader(
+  dataset=dataset,
+  batch_size=batch_size,
+  shuffle=True,
+  num_workers=0
+)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -93,34 +95,34 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Train the model
 for epoch in range(num_epochs):
-    for (words, labels) in train_loader:
-        words = words.to(device)
-        labels = labels.to(dtype=torch.long).to(device)
-        
-        # Forward pass
-        outputs = model(words)
-        # if y would be one-hot, we must apply
-        # labels = torch.max(labels, 1)[1]
-        loss = criterion(outputs, labels)
-        
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-    if (epoch+1) % 100 == 0:
-        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+  for (words, labels) in train_loader:
+    words = words.to(device)
+    labels = labels.to(dtype=torch.long).to(device)
+    
+    # Forward pass
+    outputs = model(words)
+    # if y would be one-hot, we must apply
+    # labels = torch.max(labels, 1)[1]
+    loss = criterion(outputs, labels)
+    
+    # Backward and optimize
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+      
+  if (epoch+1) % 100 == 0:
+    print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 
 print(f'final loss: {loss.item():.4f}')
 
 data = {
-"model_state": model.state_dict(),
-"input_size": input_size,
-"hidden_size": hidden_size,
-"output_size": output_size,
-"all_words": all_words,
-"tags": tags
+  "model_state": model.state_dict(),
+  "input_size": input_size,
+  "hidden_size": hidden_size,
+  "output_size": output_size,
+  "all_words": all_words,
+  "tags": tags
 }
 
 FILE = "data.pth"
