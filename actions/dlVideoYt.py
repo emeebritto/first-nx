7cbe092013128007c4621a20b8d74b43abdda2db
Emerson-Britto <emersonbritto987@gmail.com>
2022-09-14 05:34:42 -0300

    [feat]: video download system

--- actions/dlVideoYt.py
+++ actions/dlVideoYt.py
@@ -1,3 +1,9 @@
+from pytube import YouTube
+
 def dlvideoyt(svars, actions):
-  print(svars)
-  return "text", "downloading.."
+  video = YouTube(svars.get("URL"))
+  streams = video.streams.filter(type="video")
+  streams = streams.filter(progressive=True, file_extension="mp4").order_by('resolution')
+  stream_target = streams[0]
+
+  return "document", stream_target.url
