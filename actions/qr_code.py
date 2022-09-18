af31b741614d7510c0e69a828665aab1013b70ce
Emerson-Britto <emersonbritto987@gmail.com>
2022-09-18 10:25:06 -0300

    [feat]: remove stocked qrcode images

--- actions/qr_code.py
+++ actions/qr_code.py
@@ -1,12 +1,16 @@
 import qrcode
+import uuid
+import os
 
 
 def create_qr_code(svars, actions, nexa):
 	data = svars.get("DATA")
-	imgPath = "qrcode.png"
+	imgPath = f"{str(uuid.uuid4())}.png"
 	qr = qrcode.QRCode(version=15, box_size=10, border=5)
 	qr.add_data(data)
 	qr.make(fit=True)
 	img = qr.make_image(fill="black", back_color="white")
 	img.save(imgPath)
-	return [{ "resType": "photo", "res": open(imgPath, "rb") }]
+	imgBin = open(imgPath, "rb")
+	os.remove(imgPath)
+	return [{ "resType": "photo", "res": imgBin }]
