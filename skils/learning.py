7ebc99ca88e0fe3046f65cf7793f141639505c83
Emerson-Britto <emersonbritto987@gmail.com>
2023-01-30 04:04:13 -0300

    [feat]: time tracker (training)

--- skils/learning.py
+++ skils/learning.py
@@ -1,14 +1,15 @@
-import nltk
-import torch
-import json
-import re
-import numpy as np
-import torch.nn as nn
-from collections import deque
-from utils.functions import hashl
-from torch.utils.data import DataLoader
 from utils.nltk_utils import bag_of_words, tokenords, removeVars, stem
+from torch.utils.data import DataLoader
 from utils.datasets import ChatDataset
+from utils.functions import hashl
+from collections import deque
+import torch.nn as nn
+import numpy as np
+import torch
+import tqdm
+import json
+import nltk
+import re
 
 
 nltk.download('punkt', quiet=True)
@@ -80,28 +81,24 @@ class Learning:
 		return X
 
 
-	def prepareData(self, data):
-		def join(pattern, tag):
-			w = tokenords(pattern)
-			self.all_words.extend(w)
-			return (w, tag)
+	def organizeIOSeq(self, data):
+		def join(val, iOut):
+			words = tokenords(val)
+			self.all_words.extend(words)
+			return (words, iOut)
 
-		xy = []
+		io_bag = []
 		for intent in data:
-			tag = intent[self.output_name]
-			if isinstance(tag, list):
-				for out in tag:
-					self.tags.append(out)
-			else:
-				self.tags.append(tag)
+			iOut = intent[self.output_name]
+			self.tags.extend(iOut if isinstance(iOut, list) else [iOut])
 
 			iInput = intent[self.input_name]
 			if isinstance(iInput, list):
-				for idx, pattern in enumerate(iInput):
-					xy.append(join(pattern, tag[idx])) # TEMP =============
-					# xy.append(join(pattern, tag)) # TEMP =============
+				for idx, val in enumerate(iInput):
+					target_out = iOut[idx] if isinstance(iOut, list) else iOut
+					io_bag.append(join(val, target_out))
 			else:
-				xy.append(join(iInput, tag))
+				io_bag.append(join(iInput, iOut))
 
 		self.all_words = [stem(w) for w in self.all_words if w not in self.ignore_words]
 		self.all_words = sorted(set(self.all_words))
@@ -111,7 +108,7 @@ class Learning:
 		except: pass
 
 		print("bag_of_words (length): ", len(self.all_words))
-		return xy
+		return io_bag
 
 
 	def splitTrainingData(self, xy):
@@ -134,8 +131,8 @@ class Learning:
 
 	def train(self):
 		intents = self._loadIntents()
-		xy_bag = self.prepareData(data=self.intents)
-		X_train, y_train = self.splitTrainingData(xy_bag)
+		io_bag = self.organizeIOSeq(data=self.intents)
+		X_train, y_train = self.splitTrainingData(io_bag)
 
 		self.input_size = len(X_train[0])
 		self.output_size = len(self.tags)
@@ -158,9 +155,7 @@ class Learning:
 		)
 		losses = deque([], maxlen=15)
 
-		print(self.all_words)
-
-		for epoch in range(self.num_epochs):
+		for epoch in tqdm.tqdm(range(self.num_epochs), mininterval=10., desc='training'):
 			for (words, labels) in train_loader:
 				words = words.to(device)
 				labels = labels.to(dtype=torch.long).to(device)
@@ -171,7 +166,7 @@ class Learning:
 				loss.backward()
 				optimizer.step()
 
-			if (epoch + 1) % 100 == 0:
+			if (epoch + 1) % 200 == 0:
 				epochLoss = "%.4f" % loss.item()
 				losses.append(epochLoss)
 				print(f'Epoch [{epoch+1}/{self.num_epochs}], Loss: {epochLoss}', flush=True)
@@ -185,10 +180,6 @@ class Learning:
 	def probability(self, output, predicted):
 		tag = self.tags[predicted.item()]
 		probs = torch.softmax(output, dim=1)
-		# print("tags", self.tags)
-		# print("softmax", probs)
-		# print("max", predicted)
-		# print("sigmoid", torch.sigmoid(output))
 		prob = probs[0][predicted.item()]
 		return tag, prob.item()
 
